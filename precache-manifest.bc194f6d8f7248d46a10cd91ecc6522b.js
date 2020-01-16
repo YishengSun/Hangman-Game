1b755824c720ff452923e1ad106b3d664ce62b68
YishengSun <yisheng4@illinois.edu>
2020-01-15 23:44:52 -0600

    Updates

---
+++ precache-manifest.bc194f6d8f7248d46a10cd91ecc6522b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d65b228f91693735ac15c2a460a5d5a",
+    "url": "/Hangman-Game/index.html"
+  },
+  {
+    "revision": "857b910ea30e315977ac",
+    "url": "/Hangman-Game/static/css/main.7c56119f.chunk.css"
+  },
+  {
+    "revision": "150b985489ed6082c750",
+    "url": "/Hangman-Game/static/js/2.b3337e4c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Hangman-Game/static/js/2.b3337e4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "857b910ea30e315977ac",
+    "url": "/Hangman-Game/static/js/main.4eefbd62.chunk.js"
+  },
+  {
+    "revision": "297b7853b26cfc08524a",
+    "url": "/Hangman-Game/static/js/runtime-main.289146ea.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/Hangman-Game/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/Hangman-Game/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/Hangman-Game/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/Hangman-Game/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/Hangman-Game/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/Hangman-Game/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/Hangman-Game/static/media/6.c80394c2.jpg"
+  }
+]);
